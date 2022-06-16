0b9a05608595bc224ec0d2b8d50c1df9aabf2aa0
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-06-15 20:46:13 -0700

    add questionList16.js

---
+++ src/objectLists/questionList16.js
@@ -0,0 +1,98 @@
+export questionList16 = [
+  {
+    number: 1,
+    question: 'Do you find yourself doing the same activities over and over?',
+    answer: '',
+    traits: [ 'solo', 'novelty', 'active', 'specific' ]
+  },
+  {
+    number: 2,
+    question: 'Do you like to learn a lot of information about one subject?',
+    answer: '',
+    traits: [ 'social', 'novelty', 'active', 'specific' ]
+  },
+  {
+    number: 3,
+    question: 'Do you like having a person, thing, or place to take care of?',
+    answer: '',
+    traits: [ 'solo', 'nostalgia', 'active', 'specific' ]
+  },
+  {
+    number: 4,
+    question: 'Do people ask you to tell them stories from your life?',
+    answer: '',
+    traits: [ 'social', 'nostalgia', 'active', 'specific' ]
+  },
+  {
+    number: 5,
+    question: 'Is it important that others think you have good taste?',
+    answer: '',
+    traits: [ 'solo', 'novelty', 'passive', 'specific' ]
+  },
+  {
+    number: 6,
+    question: 'When you find something you like, does it consume all your attention?',
+    answer: '',
+    traits: [ 'social', 'novelty', 'passive', 'specific' ]
+  },
+  {
+    number: 7,
+    question: 'Do you like to dive deep into the context behind things?',
+    answer: '',
+    traits: [ 'solo', 'nostalgia', 'passive', 'specific' ]
+  },
+  {
+    number: 8,
+    question: 'Do you help keep your friends, family, or work community together?',
+    answer: '',
+    traits: [ 'social', 'nostalgia', 'passive', 'specific' ]
+  },
+  {
+    number: 9,
+    question: 'Do you like to make real or mental lists of favorite music, movies, art, etc?',
+    answer: '',
+    traits: [ 'solo', 'novelty', 'active', 'general' ]
+  },
+  {
+    number: 10,
+    question: 'Do you like to go out and meet new people?',
+    answer: '',
+    traits: [ 'social', 'novelty', 'active', 'general' ]
+  },
+  {
+    number: 11,
+    question: 'Do you like making or doing things in the traditional manner?',
+    answer: '',
+    traits: [ 'solo', 'nostalgia', 'active', 'general' ]
+  },
+  {
+    number: 12,
+    question: 'Do you like working with others to improve the world?',
+    answer: '',
+    traits: [ 'social', 'nostalgia', 'active', 'general' ]
+  },
+  {
+    number: 13,
+    question: 'Is enjoying creature comforts a priority for you?',
+    answer: '',
+    traits: [ 'solo', 'novelty', 'passive', 'general' ]
+  },
+  {
+    number: 14,
+    question: 'Do you like to have your cake, eat it too, and have other people watch you while you eat it?',
+    answer: '',
+    traits: [ 'social', 'novelty', 'passive', 'general' ]
+  },
+  {
+    number: 15,
+    question: 'Do you want to die surrounded by beautiful, unique objects?',
+    answer: '',
+    traits: [ 'solo', 'nostalgia', 'passive', 'general' ]
+  },
+  {
+    number: 16,
+    question: 'Do you feel better when you are serving a greater purpose?',
+    answer: '',
+    traits: [ 'social', 'nostalgia', 'passive', 'general' ]
+  }
+]

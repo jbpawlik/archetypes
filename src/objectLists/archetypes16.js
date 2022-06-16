feac49c347224be46a8bf3211afa79c6e63e6a67
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-06-15 20:46:34 -0700

    generate archetypes16.js and archetypes64.js

---
+++ src/objectLists/archetypes16.js
@@ -0,0 +1,98 @@
+export const archetypes16 = [
+  {
+    number: 1,
+    name: 'Obsessive',
+    description: '',
+    traits: [ 'solo', 'novelty', 'active', 'specific' ]
+  },
+  {
+    number: 2,
+    name: 'Academic',
+    description: '',
+    traits: [ 'social', 'novelty', 'active', 'specific' ]
+  },
+  {
+    number: 3,
+    name: 'Caretaker',
+    description: '',
+    traits: [ 'solo', 'nostalgia', 'active', 'specific' ]
+  },
+  {
+    number: 4,
+    name: 'Storyteller',
+    description: '',
+    traits: [ 'social', 'nostalgia', 'active', 'specific' ]
+  },
+  {
+    number: 5,
+    name: 'Aesthete',
+    description: '',
+    traits: [ 'solo', 'novelty', 'passive', 'specific' ]
+  },
+  {
+    number: 6,
+    name: 'Enthusiast',
+    description: '',
+    traits: [ 'social', 'novelty', 'passive', 'specific' ]
+  },
+  {
+    number: 7,
+    name: 'Historian',
+    description: '',
+    traits: [ 'solo', 'nostalgia', 'passive', 'specific' ]
+  },
+  {
+    number: 8,
+    name: 'Shepherd',
+    description: '',
+    traits: [ 'social', 'nostalgia', 'passive', 'specific' ]
+  },
+  {
+    number: 9,
+    name: 'Critic',
+    description: '',
+    traits: [ 'solo', 'novelty', 'active', 'general' ]
+  },
+  {
+    number: 10,
+    name: 'Socialite',
+    description: '',
+    traits: [ 'social', 'novelty', 'active', 'general' ]
+  },
+  {
+    number: 11,
+    name: 'Artisan',
+    description: '',
+    traits: [ 'solo', 'nostalgia', 'active', 'general' ]
+  },
+  {
+    number: 12,
+    name: 'Activist',
+    description: '',
+    traits: [ 'social', 'nostalgia', 'active', 'general' ]
+  },
+  {
+    number: 13,
+    name: 'Gourmand',
+    description: '',
+    traits: [ 'solo', 'novelty', 'passive', 'general' ]
+  },
+  {
+    number: 14,
+    name: 'Hedonist',
+    description: '',
+    traits: [ 'social', 'novelty', 'passive', 'general' ]
+  },
+  {
+    number: 15,
+    name: 'Curator',
+    description: '',
+    traits: [ 'solo', 'nostalgia', 'passive', 'general' ]
+  },
+  {
+    number: 16,
+    name: 'Volunteer',
+    description: '',
+    traits: [ 'social', 'nostalgia', 'passive', 'general' ]
+  }
+]

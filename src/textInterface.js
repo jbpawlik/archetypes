ee7459ebc4cc40894489eb5f3a5765e544832ed8
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-06-19 23:06:01 -0700

    limit player input; change colors; add description and shadow

--- src/textInterface.js
+++ src/textInterface.js
@@ -15,82 +15,114 @@ const playerName = rs.question("Player Name: \n");
 
 console.log(chalk.green(`Welcome ${playerName}` + `, answer these questions to receieve an archetype\n`));
 
-
-
 let playerAttributes = []
 
 for (let i = 0; i <= 15; i++) {
   console.log(chalk.blue(`${questionList16[i].question}`))
-  let response = rs.question(``)
+  let response = rs.question(``, {
+    limit: ['Yes', 'Y', 'yes', 'y', 'No', 'N', 'no', 'n'],
+    limitMessage: 'Yes or no, please.'
+  })
   if (response == 'Y' | response == 'y' | response == 'Yes' | response == 'yes') {
+    questionList16[i].answer = 'Y'
     playerAttributes.push(questionList16[i].traits[0])
     playerAttributes.push(questionList16[i].traits[1])
     playerAttributes.push(questionList16[i].traits[2])
     playerAttributes.push(questionList16[i].traits[3])
+  } else if (response == 'N' | response == 'n' | response == 'No' | response == 'no') { 
+    questionList16[i].answer = 'N'
   }
   response = ''
 }
 
 let occurrences = { };
 for (let i = 0, j = playerAttributes.length; i < j; i++) {
-   occurrences[playerAttributes[i]] = (occurrences[playerAttributes[i]] || 0) + 1;
+  occurrences[playerAttributes[i]] = (occurrences[playerAttributes[i]] || 0) + 1;
 }
 
 let playerArchetype = []
-let response = ''
+let playerOpposite = []
 
 if(occurrences.solo > occurrences.social) {
   playerArchetype.push('solo')
+  playerOpposite.push('social')
 } else if (occurrences.social > occurrences.solo) {
   playerArchetype.push('social')
+  playerOpposite.push('solo')
 } else {
-  console.log('Do you prefer the company of yourself to the company of others?')
-  response = rs.question(``)
+  console.log(chalk.blue('Do you prefer the company of yourself to the company of others?'))
+  let response = rs.question(``, {
+    limit: ['Yes', 'Y', 'yes', 'y', 'No', 'N', 'no', 'n'],
+    limitMessage: 'Yes or no, please'
+  })
   if (response == 'Y' | response == 'y' | response == 'Yes' | response == 'yes') {
     playerArchetype.push('solo')
+    playerOpposite.push('social')
   } else {
     playerArchetype.push('social')
+    playerOpposite.push('solo')
   }
 }
 
 if(occurrences.novelty > occurrences.nostalgia) {
   playerArchetype.push('novelty')
+  playerOpposite.push('nostalgia')
 } else if (occurrences.nostalgia > occurrences.novelty) {
   playerArchetype.push('nostalgia')
+  playerOpposite.push('novelty')
 } else {
-  console.log('Would you rather experience something new than revisit a favorite activity?')
-  response = rs.question(``)
+  console.log(chalk.blue('Would you rather experience something new than revisit a favorite activity?'))
+  let response = rs.question(``, {
+    limit: ['Yes', 'Y', 'yes', 'y', 'No', 'N', 'no', 'n'],
+    limitMessage: 'Yes or no, please'
+  })
   if (response == 'Y' | response == 'y' | response == 'Yes' | response == 'yes') {
     playerArchetype.push('novelty')
+    playerOpposite.push('nostalgia')
   } else {
     playerArchetype.push('nostalgia')
+    playerOpposite.push('novelty')
   }
 }
 
 if(occurrences.active > occurrences.passive) {
   playerArchetype.push('active')
+  playerOpposite.push('passive')
 } else if (occurrences.passive > occurrences.active) {
   playerArchetype.push('passive')
+  playerOpposite.push('active')
 } else {
-  console.log('Are you more Type A (go-getter) than Type B (go-with-the-flow)?')
-  response = rs.question(``)
+  console.log(chalk.blue('Are you more Type A (go-getter) than Type B (go-with-the-flow)?'))
+  let response = rs.question(``, {
+    limit: ['Yes', 'Y', 'yes', 'y', 'No', 'N', 'no', 'n'],
+    limitMessage: 'Yes or no, please'
+  })
   if (response == 'Y' | response == 'y' | response == 'Yes' | response == 'yes') {
     playerArchetype.push('active')
+    playerOpposite.push('passive')
   } else {
     playerArchetype.push('passive')
+    playerOpposite.push('active')
   }
 }
 if(occurrences.specific > occurrences.general) {
   playerArchetype.push('specific')
+  playerOpposite.push('general')
 } else if (occurrences.general > occurrences.specific) {
   playerArchetype.push('general')
+  playerOpposite.push('specific')
 } else {
-  console.log('Do you consider yourself a big-picture thinker?')
-  response = rs.question(``)
+  console.log(chalk.blue('Do you consider yourself a big-picture thinker?'))
+  let response = rs.question(``, {
+    limit: ['Yes', 'Y', 'yes', 'y', 'No', 'N', 'no', 'n'],
+    limitMessage: 'Yes or no, please'
+  })
   if (response == 'Y' | response == 'y' | response == 'Yes' | response == 'yes') {
     playerArchetype.push('general')
+    playerOpposite.push('specific')
   } else {
     playerArchetype.push('specific')
+    playerOpposite.push('general')
   }
 }
 
@@ -109,7 +141,10 @@ if(occurrences.specific > occurrences.general) {
 for(let i = 0; i <= 15; i++) {
   let archetypeArray = archetypes16[i].traits
   if (areEqual(archetypeArray, playerArchetype )) {
-    console.log(`Your archetype is ${archetypes16[i].name}`)
+    console.log(chalk.yellow(`Your archetype is ${archetypes16[i].name}\n${archetypes16[i].description}`))
+  }
+  if (areEqual(archetypeArray, playerOpposite)) {
+    console.log(chalk.green(`Your shadow archetype is ${archetypes16[i].name}\n${archetypes16[i].description}\nThis archetype is the least similar to you, but they may still make good friends or even partners.`))
   }
 }
 

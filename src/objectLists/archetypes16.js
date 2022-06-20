1dcf7a4659e97774865c40261514279ad694906d
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-06-19 23:04:24 -0700

    add descriptions

--- src/objectLists/archetypes16.js
+++ src/objectLists/archetypes16.js
@@ -2,97 +2,97 @@ export const archetypes16 = [
   {
     number: 1,
     name: 'Obsessive',
-    description: '',
+    description: 'Obsessives like getting all wrapped up in something. Their passion and intensity may attract or repel others.',
     traits: [ 'solo', 'novelty', 'active', 'specific' ]
   },
   {
     number: 2,
     name: 'Academic',
-    description: '',
+    description: 'Academics have a love of knowledge and want to be a part of the collective life of the mind.',
     traits: [ 'social', 'novelty', 'active', 'specific' ]
   },
   {
     number: 3,
     name: 'Caretaker',
-    description: '',
+    description: 'Caretakers find purpose and comfort in the nurturing of special people, places, or things.',
     traits: [ 'solo', 'nostalgia', 'active', 'specific' ]
   },
   {
     number: 4,
     name: 'Storyteller',
-    description: '',
+    description: 'Storytellers like to keep record of the lives of characters real or imagined, hoping to find a likeminded audience.',
     traits: [ 'social', 'nostalgia', 'active', 'specific' ]
   },
   {
     number: 5,
     name: 'Aesthete',
-    description: '',
+    description: 'Aesthetes treasure the creative arts and take pleasure in the creations of others. They enjoy developing new and exquisite tastes',
     traits: [ 'solo', 'novelty', 'passive', 'specific' ]
   },
   {
     number: 6,
     name: 'Enthusiast',
-    description: '',
+    description: 'Enthusiasts are deeply passionate about their interests and keep an eye out for others who may want to come together in appreciation.',
     traits: [ 'social', 'novelty', 'passive', 'specific' ]
   },
   {
     number: 7,
     name: 'Historian',
-    description: '',
+    description: 'Historians find great meaning in the examples of past people, paths, and plots. They think it is important to remember so as to never forget.',
     traits: [ 'solo', 'nostalgia', 'passive', 'specific' ]
   },
   {
     number: 8,
     name: 'Shepherd',
-    description: '',
+    description: 'Shepherds take an active role in shaping their greater community; they love loving and love to be loved.',
     traits: [ 'social', 'nostalgia', 'passive', 'specific' ]
   },
   {
     number: 9,
     name: 'Critic',
-    description: '',
+    description: 'Critics take pleasure in experiencing the world and then cataloguing their experiences. There`s no account for taste, but there should be!',
     traits: [ 'solo', 'novelty', 'active', 'general' ]
   },
   {
     number: 10,
     name: 'Socialite',
-    description: '',
+    description: 'Socialites cannot get enough of other people. They like to meet, greet (and sometimes compete) with as many people as they can.',
     traits: [ 'social', 'novelty', 'active', 'general' ]
   },
   {
     number: 11,
     name: 'Artisan',
-    description: '',
+    description: 'Artisans want to preserve traditions and keep them alive. They believe that hard work done properly will make a better world.',
     traits: [ 'solo', 'nostalgia', 'active', 'general' ]
   },
   {
     number: 12,
     name: 'Activist',
-    description: '',
+    description: 'Activists see a world that could be better than the one that exists today, and work hard to achieve it.',
     traits: [ 'social', 'nostalgia', 'active', 'general' ]
   },
   {
     number: 13,
     name: 'Gourmand',
-    description: '',
+    description: 'Gourmands know that the world is a buffet, and they did not come to eat salad. They want the best but will happily seconds of whatever they can take.',
     traits: [ 'solo', 'novelty', 'passive', 'general' ]
   },
   {
     number: 14,
     name: 'Hedonist',
-    description: '',
+    description: 'Hedonists know that life is meant to be enjoyed. The more pleasure something gives, the better it is.',
     traits: [ 'social', 'novelty', 'passive', 'general' ]
   },
   {
     number: 15,
     name: 'Curator',
-    description: '',
+    description: 'Curators find the good things in life and put them on display so that everyone can enjoy them.',
     traits: [ 'solo', 'nostalgia', 'passive', 'general' ]
   },
   {
     number: 16,
     name: 'Volunteer',
-    description: '',
+    description: 'Volunteers see the hurts and worries in the world and think of how to fix them. They believe that hard work done selflessly brings personal happiness.',
     traits: [ 'social', 'nostalgia', 'passive', 'general' ]
   }
 ]
